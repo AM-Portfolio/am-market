564ae9a987d0de6a089dabf4caef8a693d885e45
Sagar <sagarroy20027@gmail.com>
2026-06-04 15:41:40 +0530

    build: add missing imports for HashMap and List in UpstoxMarketDataStreamer

--- am-market-data/market-data-provider/src/main/java/com/am/marketdata/provider/upstox/UpstoxMarketDataStreamer.java
+++ am-market-data/market-data-provider/src/main/java/com/am/marketdata/provider/upstox/UpstoxMarketDataStreamer.java
@@ -22,7 +22,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
